55914427873bfc3339c41fb2b353eb523b16304f
lindseyfeng <xinyuf@usc.edu>
2023-09-13 01:11:05 -0400

    update

--- bert/main.py
+++ bert/main.py
@@ -33,7 +33,7 @@ max_input_len = tokenizer.max_model_input_sizes['bert-base-uncased']
 
 # Tokensize and crop sentence to 510 (for 1st and last token) instead of 512 (i.e. `max_input_len`)
 def tokenize_and_crop(sentence):
-  tokens = tokenizer.tokenize(sentence, max_length=510)
+  tokens = tokenizer.tokenize(sentence, max_length=510, truncation=True)
   return tokens
 
 # Load the IMDB dataset and
