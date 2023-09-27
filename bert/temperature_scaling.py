c06e67aa7722ddb15f108fb1833b611b4b5ccf72
lindseyfeng <xinyuf@usc.edu>
2023-09-27 00:47:53 -0400

    update

--- bert/temperature_scaling.py
+++ bert/temperature_scaling.py
@@ -32,12 +32,12 @@ class ModelWithTemperature(nn.Module):
         # Expand temperature to match the size of logits
         return logits / self.temperature
    
-    def freeze_base_model(self):
-        """remember to freeze base model's parameters when training temperature scaler"""
-        self.model.eval()
-        for parameter in self.model.parameters():
-            parameter.requires_grad = False
-        return self
+    # def freeze_base_model(self):
+    #     """remember to freeze base model's parameters when training temperature scaler"""
+    #     self.model.eval()
+    #     for parameter in self.model.parameters():
+    #         parameter.requires_grad = False
+    #     return self
 
     # This function probably should live outside of this class, but whatever
     def set_temperature(self, valid_loader):
@@ -46,7 +46,6 @@ class ModelWithTemperature(nn.Module):
         We're going to set it to optimize NLL.
         valid_loader (iterator): validation set loader
         """
-        self.freeze_base_model()
         self.cuda()
         nll_criterion = nn.CrossEntropyLoss().cuda()
 
