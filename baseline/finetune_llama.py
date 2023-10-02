68947805d2a686408a222eec3fedbe3bc50550e3
lindseyfeng <xinyuf@usc.edu>
2023-10-02 00:45:35 -0400

    update

--- baseline/finetune_llama.py
+++ baseline/finetune_llama.py
@@ -18,19 +18,19 @@ if tokenizer.pad_token is None:
 data = load_dataset("imdb")
 
 def tokenize_function(examples):
-    return tokenizer(examples['text'], padding='max_length', truncation=True, max_length=512)
+    return tokenizer(examples['text'], padding='max_length', truncation=True, max_length=510)
 
 tokenized_datasets = data.map(tokenize_function, batched=True)
-train_dataset = tokenized_datasets["train"]
-test_dataset = tokenized_datasets["test"]
+train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(20000))
+test_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(5000))
 
 print(train_dataset)
 print(test_dataset)
 
 # Define training arguments and initialize Trainer
 training_args = TrainingArguments(
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
+    per_device_train_batch_size=64,
+    per_device_eval_batch_size=64,
     num_train_epochs=1,
     evaluation_strategy="epoch",
     save_strategy="epoch",
