8fb7c991ba24851f0ad53e6ee9272421ddb433a6
lindseyfeng <xinyuf@usc.edu>
2023-10-03 02:36:10 -0400

    update

--- bert/config.py
+++ bert/config.py
@@ -11,5 +11,5 @@ N_LAYERS = 2
 BIDIRECTIONAL = True
 DROPOUT = 0.25
 
-TEXT = "The concert was a disaster, the sound quality was poor and the band played terribly."
+TEXT = "Sunshine always brings a smile to my face. The world feels full of possibilities and hope when the sky is clear, and the birds are singing. Every time I step outside and feel the warmth of the sun on my skin, I'm reminded of the beauty in everyday moments and the joy that nature can bring to our lives."
 
