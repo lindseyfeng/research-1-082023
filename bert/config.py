2285593b378510bc274086fffae5990fd6c6e060
lindseyfeng <xinyuf@usc.edu>
2023-10-05 13:26:49 -0400

    update

--- bert/config.py
+++ bert/config.py
@@ -11,5 +11,5 @@ N_LAYERS = 2
 BIDIRECTIONAL = True
 DROPOUT = 0.25
 
-TEXT = "Sunshine always brings a smile to my face. The world feels full of possibilities and hope when the sky is clear, and the birds are singing. Every time I step outside and feel the warmth of the sun on my skin, I'm reminded of the beauty in everyday moments and the joy that nature can bring to our lives."
+TEXT = "Life is utterly unbearable, filled with endless sorrow and despair."
 
