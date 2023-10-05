b24c3990fc1fa44c9cfaff2926e50cded96cb360
lindseyfeng <xinyuf@usc.edu>
2023-10-05 03:50:40 -0400

    update

--- baseline/raft_baseline.py
+++ baseline/raft_baseline.py
@@ -39,6 +39,7 @@ prefix = "complete the following: "
 def truncate_add_instruction_and_tokenize(batch):
     # Add prefix and truncate the first 64 tokens
     modified_texts = [prefix + ' '.join(tokenizer.tokenize(text)[64:]) for text in batch['text']]
+    print(modified_texts)
     input = tokenizer(modified_texts, truncation=True, padding='max_length', max_length=512, return_tensors="pt")
     return input
 
