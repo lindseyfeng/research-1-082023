2c3db6a69dcc279012a341fdbd32283e022a02c9
lindseyfeng <xinyuf@usc.edu>
2023-10-08 15:02:48 -0400

    update

--- baseline/raft_baseline.py
+++ baseline/raft_baseline.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
             per_device_train_batch_size=16,
             gradient_accumulation_steps=4,
             per_device_eval_batch_size=64,
-            num_train_epochs=5,
+            num_train_epochs=1,
             evaluation_strategy="epoch",
             save_strategy="epoch",
             logging_dir='./logs',
