59cdb32796d3d3e9bbf8c98d7992a733fc81f257
lindseyfeng <xinyuf@usc.edu>
2023-10-19 12:48:58 -0400

    update

--- baseline/raft_baseline.py
+++ baseline/raft_baseline.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
                 diverse_score = distinct_n_sentence_level(text,5)
                 pq.push(text, score, diverse_score)
         #train
-        training_dataset = [pq.pop() for _ in range(math.floor((len(pq)*0.2)))] 
+        training_dataset = [pq.pop() for _ in range(math.floor((len(pq)*0.5)))] 
         print(training_dataset)
         dataset_dict = Dataset.from_dict({"text": training_dataset})
         tokenized_datasets_t5 = dataset_dict.map(prepare_dataset, batched=True)
