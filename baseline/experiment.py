0174a125f8d25bbcc18c664cfeb9a64f1d80c401
lindseyfeng <xinyuf@usc.edu>
2023-10-24 02:05:59 -0400

    update

--- baseline/experiment.py
+++ baseline/experiment.py
@@ -19,7 +19,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 dataset = load_dataset("imdb")
 
-saved_directory = "./t5_imdb_complete"
+saved_directory = "./t5_imdb_ppostep_10"
 model = T5ForConditionalGeneration.from_pretrained(saved_directory)
 tokenizer = T5TokenizerFast.from_pretrained(saved_directory)
 print(tokenizer)
