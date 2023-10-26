e32cb534485b71c1b40fe39ca64f06274e0fc93b
lindseyfeng <xinyuf@usc.edu>
2023-10-25 23:10:38 -0400

    update

--- baseline/experiment.py
+++ baseline/experiment.py
@@ -60,8 +60,8 @@ if __name__ == "__main__":
     tokenized_datasets = dataset.map(truncate_add_instruction_and_tokenize, batched=True)
     test_samples = list(sample for sample in tokenized_datasets["test"]) #.filter(lambda example: example['label'] == 0))
     print(test_samples)
-    random_test_samples = random.sample(test_samples, 3200) #100 testing sample
-    train_dataloader = DataLoader(random_test_samples, shuffle=True, batch_size=300, collate_fn=collate_fn) #100
+    random_test_samples = random.sample(test_samples, 100) #100 testing sample
+    train_dataloader = DataLoader(random_test_samples, shuffle=True, batch_size=10, collate_fn=collate_fn) #100
     for batch in train_dataloader:
         with torch.no_grad(): 
             print(count)
