b0e56cfd06c904968298d308442273c02d12a813
Lindsey Feng <lindseyf@ipsec-10-2-64-157.vpn.gatech.edu>
2023-11-06 01:09:39 -0500

    update

--- baseline/reward_model_experiments.py
+++ baseline/reward_model_experiments.py
@@ -19,7 +19,7 @@ model.load_state_dict(torch.load('reward_model_0.pt', map_location=device))
 if __name__ == "__main__":
     dataset = load_dataset("imdb")
     text_dataloader = DataLoader(dataset["train"].shuffle(seed=1111).select(range(256))['text'], batch_size=64, shuffle=True)
-    for sentences in train_dataloader:
+    for sentences in text_dataloader:
         oss, acc, outs = model.get_loss(sentences)
         print("train_rm: loss: {}, acc: {}". format(loss, acc))
 
