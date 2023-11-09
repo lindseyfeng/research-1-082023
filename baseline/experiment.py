20d20ad2373b500490fa4d144b99eb67c003cb73
Lindsey Feng <lindseyf@ipsec-10-2-64-157.vpn.gatech.edu>
2023-11-09 18:39:08 -0500

    update

--- baseline/experiment.py
+++ baseline/experiment.py
@@ -20,7 +20,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 dataset = load_dataset("imdb")
 
-saved_directory = "./t5_imdb_batch/reward_checkpoint-11"
+saved_directory = "./t5_imdb_batch/checkpoint-19"
 model = T5ForConditionalGeneration.from_pretrained(saved_directory)
 tokenizer = T5TokenizerFast.from_pretrained(saved_directory)
 print(tokenizer)
