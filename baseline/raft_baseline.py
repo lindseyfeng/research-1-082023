0dec97be811c4ad9883b5249c1366d0cdf15d89b
Lindsey Feng <lindseyf@ipsec-10-2-64-206.vpn.gatech.edu>
2023-11-12 16:10:02 -0500

    commit

--- baseline/raft_baseline.py
+++ baseline/raft_baseline.py
@@ -156,17 +156,17 @@ if __name__ == "__main__":
                 tokenizer = T5TokenizerFast.from_pretrained(checkpoint_folder)
             print(tokenizer)
 
-            for inp_id in input_ids:
+            for inp_id, mask in zip(input_ids, attention_mask):
                 pq = PriorityQueue()
                 input_text = tokenizer.decode(inp_id, skip_special_tokens=True)
                 for i in range(5):
-                    output = model.generate(inp_id, attention_mask=attention_mask, max_length=48, min_length=48, eos_token_id=None)
+                    output = model.generate(inp_id, attention_mask=mask, max_length=48, min_length=48, eos_token_id=None)
                     output_text = tokenizer.decode(output, skip_special_tokens=True)
                     predicted_text = input_text + output_text
                     print(predicted_text)
                     scaled_sentiment = predict_scaled_sentiment(scaled_model, bert_tokenizer, predicted_text, best_temperature)
                     print(scaled_sentiment)
-                    pq.append(predicted_text,scaled_sentiment)
+                    pq.push(predicted_text,scaled_sentiment)
                 print(pq)
                 training_dataset.append(pq.pop())
         #train
