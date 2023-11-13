7c6cb4ecd4d67211f4853cbce6f7ee33a61ad14b
Lindsey Feng <lindseyf@ipsec-10-2-64-206.vpn.gatech.edu>
2023-11-12 19:51:27 -0500

    commit

--- baseline/raft_baseline.py
+++ baseline/raft_baseline.py
@@ -137,7 +137,9 @@ best_temperature = scaled_model.temperature.item()
 
 if __name__ == "__main__":
     #infer from t5
-    dataset = random.sample(dataset["train"]["text"], 5000)
+    all_keys = list(dataset["train"].keys())
+    selected_keys = random.sample(all_keys, 5000)
+    dataset = {key: dataset["train"][key] for key in selected_keys}
     print(dataset)
     tokenized_datasets = dataset.map(truncate_add_instruction_and_tokenize, batched=True)
     print(tokenized_datasets)
