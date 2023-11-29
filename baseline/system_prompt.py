ec18a4167283bb08bdd3a88ef963e92d90fe144a
Lindsey Feng <lindseyf@ipsec-10-2-64-207.vpn.gatech.edu>
2023-11-29 04:54:10 -0500

    llama

--- baseline/system_prompt.py
+++ baseline/system_prompt.py
@@ -2,6 +2,7 @@ from transformers import pipeline
 import torch
 import random
 from datasets import load_dataset
+from transformers import AutoModel
 
 torch.backends.cuda.matmul.allow_tf32 = True
 
@@ -12,14 +13,27 @@ random.seed(42)
 dataset = load_dataset("Anthropic/hh-rlhf")["test"]["chosen"]
 selected_items = random.sample(dataset, 3000)
 
-# reward_model = 
+# Path to your finetuned model
+model_path = 'output_models/finetuned_vicuna'
+
+# Load the model
+model = AutoModel.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+reward = []
 for text in selected_items:
 
     dialogues = text.split("Assistant:")
 
     # Filter and clean up the parts spoken by the Human
     human_dialogues = [part.split("Human:")[1].strip() for part in dialogues if "Human:" in part]
-
+    string = ""
+    
     for dialogue in human_dialogues:
-        result = pipe(dialogue)
-        print(result)
+        result = pipe(dialogue)[generated_text]
+        string.append(result)
+
+    print(string)
+    tokenized_string = tokenizer(string)
+    score = model(tokenized_string)
+    reward.append(score)
