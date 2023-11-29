36ec5c6d7bfd91be9e900a73b52bd2987794bf70
Lindsey Feng <lindseyf@ipsec-10-2-64-207.vpn.gatech.edu>
2023-11-29 04:05:28 -0500

    llama

--- baseline/system_prompt.py
+++ baseline/system_prompt.py
@@ -1,13 +1,20 @@
 # Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text-generation", model="lmsys/vicuna-7b-v1.5")
 import random
 
 random.seed(42)
 
 from datasets import load_dataset
 
+pipe = pipeline("text-generation", model="lmsys/vicuna-7b-v1.5")
+
 dataset = load_dataset("Anthropic/hh-rlhf")["test"]["chosen"]
 selected_items = random.sample(dataset, 3000)
+
+reward_model = 
 for text in selected_items:
 
     dialogues = text.split("Assistant:")
@@ -15,7 +22,8 @@ for text in selected_items:
     # Filter and clean up the parts spoken by the Human
     human_dialogues = [part.split("Human:")[1].strip() for part in dialogues if "Human:" in part]
 
+    for dialogue in human_dialogues:
+        result = pipe(dialogue)
+        print(result)
+
     print(human_dialogues)
-    
-tokenizer = AutoTokenizer.from_pretrained("lmsys/vicuna-7b-v1.5")
-model = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-7b-v1.5")
