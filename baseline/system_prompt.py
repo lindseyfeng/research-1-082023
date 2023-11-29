cd12aa488eee3b622a06f34b4e25a655fee940ee
Lindsey Feng <lindseyf@ipsec-10-2-64-207.vpn.gatech.edu>
2023-11-29 03:54:36 -0500

    llama

--- baseline/system_prompt.py
+++ baseline/system_prompt.py
@@ -1,13 +1,14 @@
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import random
 
-tokenizer = AutoTokenizer.from_pretrained("lmsys/vicuna-7b-v1.5")
-model = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-7b-v1.5")
+random.seed(42)
+
 from datasets import load_dataset
 
-dataset = load_dataset("Anthropic/hh-rlhf")["chosen"]
-
-for text in dataset:
+dataset = load_dataset("Anthropic/hh-rlhf")["test"]["chosen"]
+selected_items = random.sample(dataset, 3000)
+for text in selected_items:
 
     dialogues = text.split("Assistant:")
 
@@ -15,3 +16,6 @@ for text in dataset:
     human_dialogues = [part.split("Human:")[1].strip() for part in dialogues if "Human:" in part]
 
     print(human_dialogues)
+    
+tokenizer = AutoTokenizer.from_pretrained("lmsys/vicuna-7b-v1.5")
+model = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-7b-v1.5")
