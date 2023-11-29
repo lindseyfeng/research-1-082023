bcf2ba74371ffc07a71012cc04edbc6a4df0a770
Lindsey Feng <lindseyf@ipsec-10-2-64-207.vpn.gatech.edu>
2023-11-29 03:50:57 -0500

    llama

---
+++ baseline/system_prompt.py
@@ -0,0 +1,17 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("lmsys/vicuna-7b-v1.5")
+model = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-7b-v1.5")
+from datasets import load_dataset
+
+dataset = load_dataset("Anthropic/hh-rlhf")["chosen"]
+
+for text in dataset:
+
+    dialogues = text.split("Assistant:")
+
+    # Filter and clean up the parts spoken by the Human
+    human_dialogues = [part.split("Human:")[1].strip() for part in dialogues if "Human:" in part]
+
+    print(human_dialogues)
