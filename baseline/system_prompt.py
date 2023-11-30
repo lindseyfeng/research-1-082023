5a4627c6e0743e720c134218944b63288eeeeb70
Lindsey Feng <lindseyf@ipsec-10-2-64-207.vpn.gatech.edu>
2023-11-29 19:54:27 -0500

    llama

--- baseline/system_prompt.py
+++ baseline/system_prompt.py
@@ -74,6 +74,7 @@ for text in selected_items:
         # Get the assistant's response and append it with the prefix
         input_ids = tokenizer.encode(dialogue, return_tensors='pt')
         output = model.generate(input_ids, max_length=50, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+        print(output)
         generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
         print(generated_text)
         formatted_response += "###human: " + dialogue
