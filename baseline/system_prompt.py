3e2147bd0c76a5ddd394b2a17eb0aea98a6d2c12
Lindsey Feng <lindseyf@ipsec-10-2-192-83.vpn.gatech.edu>
2023-12-04 15:58:52 -0500

    update

--- baseline/system_prompt.py
+++ baseline/system_prompt.py
@@ -69,7 +69,8 @@ def process_batch(batch):
     outputs = model.generate(input_ids, max_length=500, pad_token_id=tokenizer.eos_token_id).to(device)
     generated_texts = tokenizer.batch_decode(outputs, skip_special_tokens=True)
     print("Input IDs device:", input_ids.device)
-    formatted_responses = ["###human: " + prompt + " ###assistant: " + generated_text[len(prompt):] for prompt, generated_text in zip(prompts, generated_texts)]
+    print(generated_texts)
+    # formatted_responses = ["###human: " + prompt + " ###assistant: " + generated_text[len(prompt):] for prompt, generated_text in zip(prompts, generated_texts)]
     print(formatted_responses)
     pipe_outputs = rm_pipe(formatted_responses, **pipe_kwargs)
     rewards = [output[0]["score"] for output in pipe_outputs]
