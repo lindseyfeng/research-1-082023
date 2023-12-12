41f9e9f374265ff150a4a07a998540ab504a568c
Lindsey Feng <lindseyf@ipsec-10-2-64-207.vpn.gatech.edu>
2023-12-12 00:34:50 -0500

    update

--- baseline/analysis_sys_prompt_gen.py
+++ baseline/analysis_sys_prompt_gen.py
@@ -47,7 +47,7 @@ system_prompt = [
 
 # Process a batch of dialogues 
 def process_batch(batch, tokenizer, model, rm_pipe, pipe_kwargs, device):
-    prompts = [system_prompt[0]+" "+text.split("Assistant:")[0].split("Human:")[1].strip() for text in batch]
+    prompts = [system_prompt[0]+" "+text[1].split("Assistant:")[0].split("Human:")[1].strip() for text in batch]
     input_ids = tokenizer(prompts, padding=True, return_tensors='pt').input_ids.to(device)
     outputs = model.generate(input_ids, min_length=200, max_length=600, pad_token_id=tokenizer.eos_token_id).to(device)
     generated_texts = tokenizer.batch_decode(outputs, skip_special_tokens=True)
@@ -79,6 +79,7 @@ def evaluate_samples(samples):
     all_responses = []
     for i in range(0, len(samples), batch_size):
         batch = samples[i:i+batch_size]
+        print(batch)
         batch_rewards, batch_responses = process_batch(batch, tokenizer, model, rm_pipe, pipe_kwargs, device)
         all_rewards.extend(batch_rewards)
         all_responses.extend(batch_responses)
