09961cb11009bb40d97ddb0b15e5e5491f4862b3
Lindsey Feng <lindseyf@ipsec-10-2-64-207.vpn.gatech.edu>
2023-12-12 19:57:13 -0500

    update

--- baseline/analysis_sys_prompt.py
+++ baseline/analysis_sys_prompt.py
@@ -33,15 +33,18 @@ def calculate_average_reward(reward_data):
 
 
 # Path to your JSON file
-file_path = 'prompt1_results.json'
+file_path = 'prompt0_results.json'
 
 # Load and filter the data
 data = load_data(file_path)
 top_25, mid_25, bottom_25 = filter_data_by_rewards(data)
 average_reward_top_25 = calculate_average_reward(top_25)
 print(top_25[0])
+print(average_reward_top_25)
 average_reward_mid_25 = calculate_average_reward(mid_25)
 print(mid_25[0])
+print(average_reward_mid_25)
 average_reward_bottom_25 = calculate_average_reward(bottom_25)
 print(bottom_25[-1])
+print(average_reward_bottom_25)
 
