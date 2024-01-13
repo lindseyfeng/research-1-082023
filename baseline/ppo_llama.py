9e73b4d74faa02dfc8325036107505cce4c4d9d9
Lindsey Feng <lindseyf@ipsec-10-2-64-197.vpn.gatech.edu>
2024-01-13 02:07:25 -0500

    update

--- baseline/ppo_llama.py
+++ baseline/ppo_llama.py
@@ -123,7 +123,7 @@ def collator(data):
 
 config = PPOConfig(
     steps = 2048,
-    learning_rate=5e-6,
+    learning_rate=5e-5,
     init_kl_coef = 0.1,
     log_with="wandb",
     ppo_epochs= 4,
