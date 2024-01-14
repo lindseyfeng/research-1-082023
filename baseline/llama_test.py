c5b1121e8f51dc65a917c5ce860828c52c89e54f
Lindsey Feng <lindseyf@ipsec-10-2-64-197.vpn.gatech.edu>
2024-01-14 11:48:17 -0500

    update

--- baseline/llama_test.py
+++ baseline/llama_test.py
@@ -41,9 +41,9 @@ print(num_batches)
 
 def process_batch(batch):
     prompts = [text.split("Assistant:")[0].split("Human:")[1].strip() for text in batch]
-    input_ids = tokenizer(prompts, padding=True, return_tensors='pt').input_ids.to(device)
+    input_ids = base_tokenizer(prompts, padding=True, return_tensors='pt').input_ids.to(device)
     outputs = base_model.generate(input_ids, min_length = 200, max_length=600, pad_token_id=tokenizer.eos_token_id).to(device)
-    generated_texts = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    generated_texts = base_tokenizer.batch_decode(outputs, skip_special_tokens=True)
     formatted_responses = ["###human: " + prompt + " ###assistant: " + generated_text[len(prompt):] for prompt, generated_text in zip(prompts, generated_texts)]
     pipe_outputs = rm_pipe(formatted_responses, **pipe_kwargs)
     rewards = [output[0]["score"] for output in pipe_outputs]
