031e639c89e2886a9f90466d685a13e3d9df8608
Lindsey Feng <lindseyf@ipsec-10-2-64-197.vpn.gatech.edu>
2024-01-14 11:58:28 -0500

    update

--- baseline/llama_test.py
+++ baseline/llama_test.py
@@ -33,7 +33,7 @@ pipe_kwargs = {
     }
 
 random.seed(42)
-test_dataset = load_dataset("Anthropic/hh-rlhf", split="train")
+test_dataset = load_dataset("Anthropic/hh-rlhf")["test"]["chosen"]
 selected_items = random.sample(test_dataset, 1000)
 batch_size = 20
 num_batches = len(selected_items) // batch_size
