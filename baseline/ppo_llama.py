35ae88693f2c7ebe4aa265a546d1908e40738837
Lindsey Feng <lindseyf@ipsec-10-2-64-197.vpn.gatech.edu>
2024-01-14 23:08:50 -0500

    update

--- baseline/ppo_llama.py
+++ baseline/ppo_llama.py
@@ -58,7 +58,7 @@ def remove_tags(text):
     :return: str, the text with specified substrings removed
     """
     # Remove "###Assistant"
-    text = text.replace("###Assistant", "")
+    text = text.replace("###Assistant: ", "")
 
     # Remove any substring starting with "###human"
     text = re.sub(r'###Human.*$', '', text, flags=re.IGNORECASE)
@@ -245,7 +245,7 @@ for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
     response = [remove_tags(r) for r in batch["response"]]
     texts = ["###Human: " + q +" ###Assistant: "+ r for q, r in zip(batch["query"], response)]
     print(texts)
-    response_tensors = tokenizer(response, padding=True, return_tensors="pt").input_ids
+    response_tensors = [torch.tensor(tokenizer.encode(r)) for r in response]
     print(response_tensors)
     pipe_outputs = rm_pipe(texts, **pipe_kwargs)
     tensor_rewards = [torch.tensor(output[0]["score"], dtype=torch.float32) for output in pipe_outputs]
