6d24fd8abbdf271f15aaadc553e0871d4e3f4c66
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-01-16 22:32:01 -0500

    update

--- baseline/ppo_llama.py
+++ baseline/ppo_llama.py
@@ -140,9 +140,9 @@ config = PPOConfig(
     init_kl_coef = 0.1,
     log_with="wandb",
     ppo_epochs= 8,
-    batch_size = 256,
-    mini_batch_size = 32,
-    gradient_accumulation_steps = 2,
+    batch_size = 32,
+    mini_batch_size = 8,
+    gradient_accumulation_steps = 8,
     )
   
 
