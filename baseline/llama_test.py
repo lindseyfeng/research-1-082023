7e08f906c1d22ad8ee6a2de2d8ec6845840cbfee
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-01-18 12:14:05 -0500

    update

--- baseline/llama_test.py
+++ baseline/llama_test.py
@@ -3,12 +3,10 @@ import random
 import torch
 from datasets import load_dataset
 from statistics import mean
-from trl import PPOTrainer, set_seed, AutoModelForCausalLMWithValueHead
 
-ppo_dir = "./llama_ppo_step5000step_2400"
-# ppo_dir = "./checkpoints/checkpoint-1000"
-# ppo_dir = "./finetuned_llama_ppostep_1800"
-# ppo_dir = "./LMFlow/output_models/finetuned_llama2"
+# ppo_dir = "./llama_ppo_step5000step_2400"
+ppo_dir = "./checkpoints/checkpoint-1000"
+sft_model_dir = "./LMFlow/output_models/finetuned_llama2"
 base_dir = "../../llama/llama-2-7b"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 # base_model = LlamaForCausalLM.from_pretrained(base_dir).to(device)
@@ -19,17 +17,15 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 # sft_tokenizer = LlamaTokenizer.from_pretrained(sft_model_dir)
 # sft_tokenizer.pad_token_id=sft_tokenizer.eos_token_id
 # sft_tokenizer.padding_side = 'left'
-model = AutoModelForCausalLMWithValueHead.from_pretrained(ppo_dir).to(device)
-tokenizer = LlamaTokenizer.from_pretrained(ppo_dir)
-tokenizer.pad_token_id=tokenizer.eos_token_id
-# tokenizer.padding_side = 'left'
+model = LlamaForCausalLM.from_pretrained(base_dir).to(device)
+tokenizer = LlamaTokenizer.from_pretrained(base_dir)
 
-rm_tokenizer = AutoTokenizer.from_pretrained("weqweasdas/hh_rlhf_rm")
+rm_tokenizer = AutoTokenizer.from_pretrained("weqweasdas/hh_rlhf_rm_open_llama_3b")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 rm_pipe = pipeline(
       "sentiment-analysis",
-      model="weqweasdas/hh_rlhf_rm",
+      model="weqweasdas/hh_rlhf_rm_open_llama_3b",
       device=device,
       tokenizer=rm_tokenizer,
       model_kwargs={"torch_dtype": torch.bfloat16}
@@ -41,35 +37,23 @@ pipe_kwargs = {
       "batch_size": 1
     }
 
-generation_kwargs = {
-    "min_length": -1,
-    "top_k": 0.0,
-    "top_p": 1.0,
-    "do_sample": True,
-    "pad_token_id": tokenizer.pad_token_id,
-    "eos_token_id": -1,
-    "max_new_tokens": 100,
-}
 random.seed(1111)
-set_seed(1111)
-ppo_trainer = PPOTrainer(model)
 test_dataset = load_dataset("Anthropic/hh-rlhf")["test"]["chosen"]
 selected_items = random.sample(test_dataset, 1000)
 batch_size = 5
 num_batches = len(selected_items) // batch_size
 print(num_batches)
+
 def process_batch(batch):
     prompts = [text.split("Assistant:")[0].split("Human:")[1].strip() for text in batch]
-    input_ids = tokenizer(prompts, padding=True, return_tensors='pt').input_ids.to(device)
-    outputs = PPOTrainer.generate(input_ids, 
-    return_prompt=False,
-     **generation_kwargs,).to(device)
-    generated_texts = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    print(prompts)
+    input_ids = ppo_tokenizer(prompts, return_tensors='pt').input_ids.to(device)
+    outputs = ppo_model.generate(input_ids).to(device)
+    generated_texts = ppo_tokenizer.batch_decode(outputs, skip_special_tokens=True)
     formatted_responses = ["###human: " + prompt + " ###assistant: " + generated_text[len(prompt):] for prompt, generated_text in zip(prompts, generated_texts)]
     pipe_outputs = rm_pipe(formatted_responses, **pipe_kwargs)
     rewards = [output[0]["score"] for output in pipe_outputs]
     print("batch_avg: {}".format(mean(rewards)))
-    print(formatted_responses)
     return rewards, formatted_responses
 
 # Process all batches and calculate the average reward
@@ -83,4 +67,4 @@ for i in range(num_batches):
 print(len(all_rewards))
 average_reward = mean(all_rewards)
 print("Average Reward:", average_reward)
-print(all_responses)
+print(all_responses[100])
