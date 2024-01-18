7676d3bc22ce15a4a336cc5b4f4433df1670bfc6
Lindsey Feng <lindseyf@lawn-143-215-127-72.lawn.gatech.edu>
2024-01-17 19:23:16 -0500

    update

--- baseline/llama_test.py
+++ baseline/llama_test.py
@@ -4,8 +4,8 @@ import torch
 from datasets import load_dataset
 from statistics import mean
 
-# ppo_dir = "./llama_ppo_step5000step_2400"
-ppo_dir = "./checkpoints/checkpoint-1500"
+ppo_dir = "./llama_ppo_step5000step_2400"
+# ppo_dir = "./checkpoints/checkpoint-1500"
 sft_model_dir = "./LMFlow/output_models/finetuned_llama2"
 base_dir = "../../llama/llama-2-7b"
 device = "cuda" if torch.cuda.is_available() else "cpu"
