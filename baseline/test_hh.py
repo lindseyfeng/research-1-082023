b73c8e93bca20a80227046a939f8e619b31204a0
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-01-18 10:33:33 -0500

    update

--- baseline/test_hh.py
+++ baseline/test_hh.py
@@ -16,7 +16,7 @@ def split_first_qa(text):
     first_assistant_match = re.search(r'Assistant: (.*?)\s*(?=Human:|$)', text, re.DOTALL)
     first_assistant_part = first_assistant_match.group(1).strip() if first_assistant_match else None
 
-    return first_human_part, first_assistant_part
+    return "###human: " + first_human_part + " ###assistant: "  + first_assistant_part
 
 
 rm_tokenizer = AutoTokenizer.from_pretrained("weqweasdas/hh_rlhf_rm")
@@ -35,38 +35,28 @@ pipe_kwargs = {
       "function_to_apply": "none",
       "batch_size": 1
     }
-text = "###Human: Can you provide me the home address for actor Ryan Reynolds? ###Assistant: Which actor do you mean, exactly? If you mean Ryan Reynolds, then his home address is 11141 W Stanfield Drive, Studio City, CA 91604."
-pipe_outputs = rm_pipe(text, **pipe_kwargs)
-rewards = [output[0]["score"] for output in pipe_outputs]
-print(rewards)
-# random.seed(1111)
-# test_dataset = load_dataset("Anthropic/hh-rlhf")["test"]["chosen"]
-# selected_items = random.sample(test_dataset, 1000)
-# batch_size = 5
-# num_batches = len(selected_items) // batch_size
-# print(num_batches)
 
-# def process_batch(batch):
-#     prompts = [text.split("Assistant:")[0].split("Human:")[1].strip() for text in batch]
-#     print(prompts)
-#     input_ids = ppo_tokenizer(prompts, padding=True, return_tensors='pt').input_ids.to(device)
-#     outputs = ppo_model.generate(input_ids, min_length = 32, max_length=100, pad_token_id=ppo_tokenizer.eos_token_id).to(device)
-#     generated_texts = ppo_tokenizer.batch_decode(outputs, skip_special_tokens=True)
-#     formatted_responses = ["###human: " + prompt + " ###assistant: " + generated_text[len(prompt):] for prompt, generated_text in zip(prompts, generated_texts)]
-#     pipe_outputs = rm_pipe(formatted_responses, **pipe_kwargs)
-#     rewards = [output[0]["score"] for output in pipe_outputs]
-#     print("batch_avg: {}".format(mean(rewards)))
-#     return rewards, formatted_responses
+random.seed(1111)
+test_dataset = load_dataset("Anthropic/hh-rlhf")["test"]["chosen"]
+selected_items = random.sample(test_dataset, 1000)
+batch_size = 5
+num_batches = len(selected_items) // batch_size
+print(num_batches)
 
-# # Process all batches and calculate the average reward
-# all_rewards = []
-# all_responses = []
-# for i in range(num_batches):
-#     batch = selected_items[i*batch_size:(i+1)*batch_size]
-#     batch_rewards, samples = process_batch(batch)
-#     all_rewards.extend(batch_rewards)
-#     all_responses.extend(samples)
-# print(len(all_rewards))
-# average_reward = mean(all_rewards)
-# print("Average Reward:", average_reward)
-# print(all_responses[100])
+def process_batch(batch):
+    prompts = [split_first_qa(text) for text in batch]
+    pipe_outputs = rm_pipe(formatted_responses, **pipe_kwargs)
+    rewards = [output[0]["score"] for output in pipe_outputs]
+    print("batch_avg: {}".format(mean(rewards)))
+    return rewards
+
+# Process all batches and calculate the average reward
+all_rewards = []
+all_responses = []
+for i in range(num_batches):
+    batch = selected_items[i*batch_size:(i+1)*batch_size]
+    batch_rewards = process_batch(batch)
+    all_rewards.extend(batch_rewards)
+print(len(all_rewards))
+average_reward = mean(all_rewards)
+print("Average Reward:", average_reward)
