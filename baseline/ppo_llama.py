4053b3278e6643da48075fa9a9c059488896d09c
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-01-21 13:29:33 -0500

    update

--- baseline/ppo_llama.py
+++ baseline/ppo_llama.py
@@ -136,7 +136,7 @@ def collator(data):
 
 config = PPOConfig(
     steps = 1024,
-    learning_rate=1e-6,
+    learning_rate=1e-5,
     init_kl_coef = 0.1,
     log_with="wandb",
     ppo_epochs= 4,
